7fd1d39bfbf01c8be7d31064001172805c10b795
ElkinXD <elkindavidvasquezrivera801@gmail.com>
2024-10-04 14:34:12 -0500

    Definicion y sobreescritrua en clase Camera

--- cameras.py
+++ cameras.py
@@ -1,11 +1,18 @@
 class Camera:
-    def __init__(self, ip, regla_1, in_1, regla_2, in_2):
+    def __init__(self, id, ip, usuario, clave, in1, in2, out1, out2):
+        self.id = id
         self.ip = ip
-        self.regla_1 = regla_1
-        self.in_1 = in_1
-        self.regla_2 = regla_2
-        self.in_2 = in_2
+        self.usuario = usuario
+        self.clave = clave
+        self.in1 = in1
+        self.in2 = in2
+        self.out1 = out1
+        self.out2 = out2
 
     def __str__(self):
         return f'{self.ip}-{self.regla_1}-{self.in_1}-{self.regla_2}'
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.id == other.id and self.ip == other.ip
 
